14a90636766e9ed1acce85a00540a4d9dba1ed86
JhordyCastro <ja.castro@campus.fct.unl.pt>
2018-10-01 00:17:18 +0100

    ood

--- Lab2_Analyser.java
+++ Lab2_Analyser.java
@@ -12,12 +12,16 @@ import java.util.Arrays;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.commongrams.CommonGramsFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.ngram.EdgeNGramTokenFilter;
 import org.apache.lucene.analysis.ngram.NGramTokenFilter;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
 
 /**
  * @author jmag
@@ -56,15 +60,15 @@ public class Lab2_Analyser extends Analyzer {
 		
 		TokenStream tok = null;
 		tok = new StandardFilter(src);					// text into non punctuated text
-//		tok = new LowerCaseFilter(tok);					// changes all texto into lowercase
+		tok = new LowerCaseFilter(tok);					// changes all texto into lowercase
 //		tok = new StopFilter(tok, stopSet);				// removes stop words
 
 //		tok = new ShingleFilter(tok, 2, 3);				// creates word-grams with neighboring works
-//		tok = new CommonGramsFilter(tok, stopwords);	// creates word-grams with stopwords
+//		tok = new CommonGramsFilter(tok, stopSet);	// creates word-grams with stopwords
 
-// 		tok = new NGramTokenFilter(tok,2,5);			// creates unbounded n-grams
+//		tok = new NGramTokenFilter(tok,2,5);			// creates unbounded n-grams
 //		tok = new EdgeNGramTokenFilter(tok,2,5);		// creates word-bounded n-grams
-
+ 		
 //		tok = new SnowballFilter(tok, "English");		// stems workds according to the specified language
 		
 		return new TokenStreamComponents(src, tok) {
@@ -87,7 +91,7 @@ public class Lab2_Analyser extends Analyzer {
 	// Test the different filters
 	public static void main(String[] args) throws IOException {
 
-		final String text = "This is a demonstration, of the TokenStream Lucene-API,";
+		final String text = "5,23,2010-07-19T19:14:43Z,3,85,<p>The R-project</p>";
 
 		Lab2_Analyser analyzer = new Lab2_Analyser();
 		TokenStream stream = analyzer.tokenStream("field", new StringReader(text));
